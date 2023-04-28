90098f3b5ebf91c88c333ba669097fb0c2ec25da
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-04-28 15:10:29 +0200

    Update index.js
    Code specification

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function digitalRoot(n) {
+	//temporary sum
 	var sum = 0;
+	//**int to array of chars
 	var arr = n.toString().split("");
 	do {
+		//sum all elements as integers
 		arr.forEach(element => (sum += parseInt(element)));
+		//clean previous array
 		arr = [];
+		//**int to array of chars from sum integer
 		arr = sum.toString().split("");
+		//temporary sum=0 for the next iteration
 		sum = 0;
 	}
+	//condition for ending loop when arr is only one cell
 	while (arr.length != 1)
+	//returning array last element as integer
 	return parseInt(arr);
 }
